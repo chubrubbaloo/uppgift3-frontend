ec7fedb1d42a790e2def7dc7f1fa4df7f9805ac6
Karl.a.Blixt <karl.a.blixt@gmail.com>
2022-11-28 12:18:30 +0100

    created user class.

--- src/entities/User.js
+++ src/entities/User.js
@@ -1,9 +1,15 @@
-
 class User{
     username
 
     constructor(username) {
         this.username = username
+    }
+
+    equals(otherUser){
+        if (!(otherUser instanceof User)){
+            return false
+        }
+        return (this.username === otherUser.username)
     }
 }
 
